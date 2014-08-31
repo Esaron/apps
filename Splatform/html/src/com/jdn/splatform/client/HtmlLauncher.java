060e6ba4fcdc08c122004e14d7fe6c0613481e5f
jdn <esaron88@gmail.com>
2014-08-30 21:09:33 -0400

    Initial commit

---
+++ Splatform/html/src/com/jdn/splatform/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.jdn.splatform.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.jdn.splatform.SplatformGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new SplatformGame();
+        }
+}
